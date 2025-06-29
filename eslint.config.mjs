bc8e4ebfdebafa6c8ba8381b98691ec99aacac14
aho4ahoaho <naoki-ah4@users.noreply.github.com>
2025-06-29 16:10:08 +0900

    chore: add ESLint configuration and dependencies
    - Created a new ESLint configuration file (eslint.config.mjs) using recommended settings from @eslint/js and typescript-eslint.
    - Added @eslint/js, eslint, and typescript-eslint as development dependencies in package.json.

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+    eslint.configs.recommended,
+    tseslint.config(
+        tseslint.configs.recommendedTypeChecked,
+        {
+            languageOptions: {
+                parserOptions: {
+                    projectService: true,
+                    tsconfigRootDir: import.meta.dirname,
+                },
+            },
+        }
+    ),
+    {
+        rules: {
+            "@typescript-eslint/no-unused-vars": "warn",
+        }
+    }
+    ,
+    globalIgnores(["./*.*", ".react-router"])
+]
+)
--- src/app/routes/home.tsx
+++ src/app/routes/home.tsx
@@ -1,6 +1,6 @@
 import type { Route } from "./+types/home";
 
-export function meta({ }: Route.MetaArgs) {
+export function meta({ params }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
