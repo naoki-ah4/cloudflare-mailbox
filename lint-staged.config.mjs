07d7ff2790f33759efe8b5f236ffe652999c9010
aho4ahoaho <naoki-ah4@users.noreply.github.com>
2025-07-04 00:57:48 +0900

    サンプルの提示

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,4 @@
 export default {
   "*.{ts,tsx}": ["eslint --fix", "prettier --write"],
-  "*.{js,jsx,mjs,cjs,json,html,css,scss,md}": ["prettier --write"],
+  "*.{js,jsx,mjs,cjs,json,jsonc,html,css,scss,md}": ["prettier --write"],
 };
