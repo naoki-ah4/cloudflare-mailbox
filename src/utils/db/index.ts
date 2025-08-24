039af1f21e30c1724a669b6bedef1cbb2eb2131d
aho4ahoaho <naoki-ah4@users.noreply.github.com>
2025-08-24 22:22:51 +0900

    refactor: KV互換のエイリアスを削除

--- src/utils/db/index.ts
+++ src/utils/db/index.ts
@@ -27,24 +27,3 @@ export * from "./settings";
 export * from "./rate-limit";
 export * from "./folder";
 export * from "./outbox";
-
-// KV互換のエイリアス
-export { UserDB as UserKV } from "./user";
-export { SessionDB as SessionKV } from "./session";
-export {
-  AdminDB as AdminKV,
-  AdminSessionDB as AdminSessionKV,
-  InviteDB as InviteKV,
-} from "./admin";
-export {
-  MessageDB as MessageKV,
-  InboxDB as InboxKV,
-  ThreadDB as ThreadKV,
-} from "./email";
-export {
-  UserSettingsDB as SettingsKV,
-  SystemSettingsDB as SystemKV,
-} from "./settings";
-export { RateLimitDB as RateLimitKV } from "./rate-limit";
-export { FolderDB as FolderKV } from "./folder";
-export { OutboxDB as OutboxKV } from "./outbox";
